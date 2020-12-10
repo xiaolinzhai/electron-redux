ef044a7f1029fe2e1117a41f539f459164c41ef7
翟晓林 <a2231232@163.com>
2020-12-10 15:25:13 +0800

    修改为不发送到主线

--- packages/electron-redux/src/middleware/forwardToMain.js
+++ packages/electron-redux/src/middleware/forwardToMain.js
@@ -2,17 +2,29 @@ import { ipcRenderer } from 'electron';
 import validateAction from '../helpers/validateAction';
 
 // eslint-disable-next-line consistent-return, no-unused-vars
-export const forwardToMainWithParams = (params = {}) => store => next => (action) => {
+export const forwardToMainWithParams = (params = {}) => store => next => action => {
   const { blacklist = [] } = params;
   if (!validateAction(action)) return next(action);
+
   if (action.meta && action.meta.scope === 'local') return next(action);
 
+  // change scope to local, both dispatch
+  const rendererAction = {
+    ...action,
+    meta: {
+      ...action.meta,
+      scope: 'local',
+    },
+  };
+  /*
+   */
   if (blacklist.some(rule => rule.test(action.type))) {
     return next(action);
+  } else {
+    // stop action in-flight
+    ipcRenderer.send('redux-action', rendererAction);
+    return next(action);
   }
-
-  // stop action in-flight
-  ipcRenderer.send('redux-action', action);
 };
 
 const forwardToMain = forwardToMainWithParams({
